7be37b6add2f6d1cb8562dcd6f0f7a9dcdf8de23
SunKnight002 <leosun6582@gmail.com>
2024-11-17 22:40:57 +0800

    Feat：切出首頁第2區塊

---
+++ src/components/Card.vue
@@ -0,0 +1,190 @@
+<template>
+    <section class="Card_Area">
+        <!-- 動態渲染卡片 -->
+        <section class="Card">
+            <div class="Card_Img" @click="commingsoon">
+                <!-- <img src="@/assets/img/Card_HolyPoem.png" alt=""> -->
+                <!-- <img :src="card.imgSrc" :alt="card.title"> -->
+                <img :src="cardData.imgSrc" :alt="cardData.title" />
+            </div>
+            <div class="Card_Info">
+                <div class="Card_Contain">
+                    <div class="Card_Title">
+                        <!-- <p>HOLY POEM</p> -->
+                        <!-- <p>{{ card.title }}</p> -->
+                        <p>{{ cardData.title }}</p>
+                    </div>
+
+                    <div class="Card_Platform">
+                        <!-- <img src="@/assets/img/Card_Info_IOSICON.png" alt="">
+                        <img src="@/assets/img/Card_Info_ANDROIDICON.png" alt="">
+                        <img src="@/assets/img/Card_Info_PCICON.png" alt=""> -->
+                        <!-- <img
+                            v-for="(platform, pIndex) in card.platforms"
+                            :key="pIndex"
+                            :src="platform.imgSrc"
+                            :alt="platform.alt"
+                        > -->
+                        <img
+                            v-for="(platform, index) in cardData.platforms"
+                            :key="index"
+                            :src="platform.imgSrc"
+                            :alt="platform.alt"
+                        />
+                    </div>
+                </div>
+
+                <div class="Card_Link">
+                    <img src="@/assets/img/Card_Info_Arrow.png" alt="">
+                </div>
+
+            </div>
+        </section>
+    </section>
+</template>
+
+<script>
+
+// 第二種方式（從完整檔案導入）
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/dist/sweetalert2.css';
+
+    export default {
+        props: {
+            cardData: {
+            type: Object,
+            required: true,
+            },
+        },
+        data() {
+        return {
+            // 動態卡片資料
+            cards: [
+                {
+                    imgSrc: "/img/Card_HolyPoem.png",
+                    title: "HOLY POEM",
+                    platforms: [
+                        { imgSrc: "/img/Card_Info_IOSICON.png", alt: "iOS Icon" },
+                        { imgSrc: "/img/Card_Info_ANDROIDICON.png", alt: "Android Icon" },
+                        { imgSrc: "/img/Card_Info_PCICON.png", alt: "PC Icon" },
+                    ],
+                },
+                {
+                    imgSrc: "/img/Card_HolePoemBorn.png",
+                    title: "HOLY POEM;BORN",
+                    platforms: [
+                        { imgSrc: "/img/Card_Info_IOSICON.png", alt: "iOS Icon" },
+                        { imgSrc: "/img/Card_Info_ANDROIDICON.png", alt: "Android Icon" },
+                    ],
+                },
+                {
+                    imgSrc: "/img/Card_DragonHeart.png",
+                    title: "DRAGON HEART",
+                    platforms: [
+                        { imgSrc: "/img/Card_Info_PCICON.png", alt: "PC Icon" },
+                    ],
+                },
+            ],
+        };
+    },
+        methods: {
+            commingsoon(){
+                console.log(Swal); // 應顯示 `fire` 函數
+                Swal.fire({
+                title: '頁面製作中',
+                text: '您好，相關頁面規劃中',
+                icon: 'info',
+                confirmButtonColor: '#FCD15B',
+                confirmButtonText: 'OK'
+                });
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped >
+@use 'sass:map';
+@use '../assets/sass/basic/_var' as var;
+@use '../assets/sass/basic/_color' as color;
+@use '../assets/sass/mixins/_minins' as minins;
+@use '../assets/sass/basic/_animate' as animate;
+
+
+    .Card_Area{
+        width: 100%;
+        margin: 0 auto;
+        display: grid;
+    }
+
+    .Card{
+        position: relative;
+        margin: 0 auto;
+    }
+
+    .Card_Img {
+        display: block;
+        cursor: pointer;
+        position: relative;
+        z-index: 1;
+
+        img{
+            display: block;
+            cursor: pointer;
+            width: auto;
+            height: auto;
+            max-width: 100%;
+        }
+    }
+
+    .Card_Info {
+        width: 100%;
+        background-color: #81007E;
+        border-radius: 0 0 60px 60px;
+        position: absolute;
+        z-index: 2;
+        top: 84%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 12px 0 12px 0;
+    }
+
+    .Card_Contain{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .Card_Link{
+        margin-left: 40px;
+    }
+
+    .Card_Title {
+        width: 100%;
+        display: flex;
+        align-items: center;
+
+
+        p {
+            font-size: 32px;
+            color: map.get(color.$colors , white );
+            font-family: "Odibee Sans", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+
+    }
+
+    .Card_Platform {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+
+        img {
+            display: block;
+            cursor: pointer;
+        }
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script>
   import Header from '@/components/Header.vue';
+  import Card from '@/components/Card.vue';
+  import gsap from 'gsap';
 
     // Import Swiper Vue.js components
     import { Swiper, SwiperSlide } from 'swiper/vue';
-    import { onMounted, onBeforeUnmount } from 'vue';
+    import { onMounted, onBeforeUnmount,ref } from 'vue';
     import { useRouter } from 'vue-router';
     // Import Swiper styles
     import 'swiper/css';
@@ -11,14 +13,63 @@
     import 'swiper/scss/pagination';
       // import required modules
     import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+    import 'swiper/css';
+    import 'swiper/scss/navigation';
+    import 'swiper/scss/pagination';
+
     export default {
         components: {
             Swiper,
             SwiperSlide,
             Header,
+            Card,
         },
         setup() {
             const router = useRouter();
+            const GameSlogan = ref(null);
+            onMounted(() => {
+              gsap.fromTo(
+                GameSlogan.value,
+                { opacity: 0, y: 50 }, // 開始狀態：透明度為 0，y 位移 50px
+                { opacity: 1, y: 0, duration: 1.5, ease: 'power2.out' } // 結束狀態：透明度 1，y 位移 0，動畫時間 1.5秒
+      );
+    });
+
+            // 卡片
+            const cards = [
+              {
+                  imgSrc: "/img/Card_HolyPoem.png",
+                  title: "TRANPSPORTE",
+                  platforms: [
+                      { imgSrc: "/img/Card_Info_IOSICON.png", alt: "iOS Icon" },
+                      { imgSrc: "/img/Card_Info_ANDROIDICON.png", alt: "Android Icon" },
+                      { imgSrc: "/img/Card_Info_PCICON.png", alt: "PC Icon" },
+                  ],
+              },
+              {
+                  imgSrc: "/img/Card_HolePoemBorn.png",
+                  title: "HOLY POEM;BORN",
+                  platforms: [
+                      { imgSrc: "/img/Card_Info_IOSICON.png", alt: "iOS Icon" },
+                      { imgSrc: "/img/Card_Info_ANDROIDICON.png", alt: "Android Icon" },
+                  ],
+              },
+              {
+                  imgSrc: "/img/Card_DragonHeart.png",
+                  title: "DRAGON HEART",
+                  platforms: [
+                      { imgSrc: "/img/Card_Info_PCICON.png", alt: "PC Icon" },
+                  ],
+              },              {
+                  imgSrc: "/img/Card_8inKey.png",
+                  title: "8 IN KEY",
+                  platforms: [
+                      { imgSrc: "/img/Card_Info_IOSICON.png", alt: "iOS Icon" },
+                      { imgSrc: "/img/Card_Info_ANDROIDICON.png", alt: "Android Icon" },
+                  ],
+              },
+          ];
 
             const initSwiper = () => {
             // 您可以使用 Swiper 的 API 在這裡初始化或更新 Swiper
@@ -54,14 +105,16 @@
                 router.afterEach(() => {});
             });
 
-            return { onSwiper, onSlideChange, Navigation, Pagination, Autoplay };
+            return { cards,onSwiper, onSlideChange, Navigation, Pagination, Autoplay,GameSlogan };
         }
     }
 </script>
 
 <template>
     <main>
+        <!-- 標頭 -->
         <Header/>
+        <!-- 首頁第一層輪播，播放歷年作品 -->
         <section class="GameHex_Index_Swiper">
             <!-- Swiper -->
             <Swiper
@@ -84,6 +137,36 @@
               <!-- 自定義導航按鈕 -->
               <div class="swiper-button-next-custom"></div>
               <div class="swiper-button-prev-custom"></div>
+        </section>
+
+        <!-- 首頁第二層歷年產品圖卡 -->
+        <section class="GameHex_Index_Card">
+          <h1>Product</h1>
+          <img src="/img/Span_Light.png" alt="發光橘色條">
+          <Swiper
+              class="mySwiperCard"
+              :modules="[Navigation, Pagination, Autoplay]"
+              :navigation="true"
+              :pagination="{ clickable: true, type: 'bullets' }"
+              :slides-per-view="3"
+              :space-between="30"
+              :autoplay="{ delay: 3000, disableOnInteraction: false }"
+          >
+              <SwiperSlide v-for="(card, index) in cards" :key="index">
+                  <Card :card-data="card" />
+              </SwiperSlide>
+          </Swiper>
+          <p ref="GameSlogan">More than 20 Games are available on Google Play And IOS Store!</p>
+        </section>
+
+        <!-- 首頁第三層公司提供服務 -->
+        <section class="GameHex_Index_Services">
+          <div class="Page_Title">
+            <p>Product</p>
+            <img src="/img/Span_Light.png" alt="發光橘色條">
+          </div>
+
+
         </section>
     </main>
 </template>
@@ -166,7 +249,7 @@ $color_2: var(--swiper-theme-color);
       --swiper-theme-color: #fff;
   }
 
-  /* 自定義導航按鈕樣式 */
+  /* 自定義導航按鈕樣式 1*/
 .swiper-button-next-custom {
   width: 92px;
   height: 82px;
@@ -192,4 +275,114 @@ $color_2: var(--swiper-theme-color);
     z-index: 10;
     cursor: pointer;
 }
+
+// 第二區塊
+.GameHex_Index_Card {
+  padding: 140px 140px 66px 140px;
+  background-color:#10110C;
+  font-family: "Odibee Sans", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: #fff;
+
+  h1{
+    font-size: 32px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  p{
+    font-size: 40px;
+    margin: 0 auto;
+    text-align: center;
+    margin-top: 36px;
+    opacity: 0;
+    transform: translateY(50px);
+  }
+
+  img{
+    text-align: center;
+    display: block;
+    margin: 20px auto 15px auto;
+  }
+}
+
+.mySwiperCard {
+  width: 70%;
+  height: 70vh;
+  margin: 0 auto;
+  position: relative;
+
+
+  .swiper-slide {
+    width: 100%;
+    height: 60vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 60px;
+    background-color: transparent;
+
+    &:hover::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(255, 255, 255, 0.1);
+      backdrop-filter: blur(60px);
+      border-radius: 60px;
+      z-index: 1;
+    }
+  }
+}
+
+/* 調整圓點的位置 */
+.swiper-pagination {
+  position: absolute;
+  bottom: 10px;  /* 調整圓點的位置 */
+  width: 100%;
+  text-align: center;
+  z-index: 5;
+}
+
+.swiper-pagination-bullet {
+  width: 12px;
+  height: 12px;
+  margin: 0 4px;
+  background-color: #fff;
+  border-radius: 50%;
+  opacity: 0.5;
+  transition: opacity 0.3s;
+}
+
+.swiper-pagination-bullet-active {
+  opacity: 1;
+}
+
+//公司提供服務GameHex_Index_Services
+
+.GameHex_Index_Services{
+  width: 100%;
+  height: 100vh;
+  padding: 80px 0 80px 0;
+  background-image: url('@/assets/img/BG_Index_Bussiness.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.Page_Title{
+  font-family: "Odibee Sans", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: #fff;
+  margin-left: 40px;
+  margin-top: 140px;
+
+  p{
+    font-size: 40px;
+  }
+}
+
 </style>
